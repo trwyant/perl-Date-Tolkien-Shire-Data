ce97646fd16738411072ccbc4d89f64e17d348f7
Tom Wyant <wyant@cpan.org>
2020-12-22 10:28:25 -0500

    Use GitHub as bug tracker. R.I.P. rt.cpan.org.

--- lib/Date/Tolkien/Shire/Data.pm
+++ lib/Date/Tolkien/Shire/Data.pm
@@ -1991,7 +1991,8 @@ L<DateTime::Fiction::JRRTolkien::Shire|DateTime::Fiction::JRRTolkien::Shire>
 =head1 SUPPORT
 
 Support is by the author. Please file bug reports at
-L<https:rt.cpan.org>, or in electronic mail to the author.
+L<https://github.com/trwyant/perl-Date-Tolkien-Shire-Data/issues>, or in
+electronic mail to the author.
 
 =head1 AUTHOR
 
