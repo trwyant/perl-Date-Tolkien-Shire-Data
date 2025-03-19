82199854dc34b8bed02a9b3e4788fc888fa730c5
Tom Wyant <wyant@cpan.org>
2025-03-19 14:07:58 -0400

    Add __search_on_date().

--- lib/Date/Tolkien/Shire/Data.pm
+++ lib/Date/Tolkien/Shire/Data.pm
@@ -32,6 +32,7 @@ our @EXPORT_OK = qw{
     __on_date __on_date_accented
     __quarter __quarter_name __quarter_abbr
     __rata_die_to_year_day
+    __search_on_date
     __trad_weekday_abbr __trad_weekday_name __trad_weekday_narrow
     __valid_date_class
     __weekday_abbr __weekday_name __weekday_narrow
@@ -56,6 +57,7 @@ our %EXPORT_TAGS = (
 use constant ARRAY_REF	=> ref [];
 use constant CODE_REF	=> ref sub {};
 use constant HASH_REF	=> ref {};
+use constant REGEX_REF	=> ref qr/foo/;
 
 use constant DAY_OF_YEAR_MIDYEARS_DAY	=> 183;
 use constant DAY_OF_YEAR_OVERLITHE	=> 184;
@@ -703,44 +705,82 @@ sub _fmt_on_date {
 	return $on_date[$month][$day];
     }
 
+    sub __search_on_date {
+	my @criteria = @_;
+	foreach ( @criteria ) {
+	    if ( ref eq CODE_REF ) {
+		# Accept as-is
+	    } elsif ( ref eq REGEX_REF ) {
+		my $criterion = $_;
+		$_ = sub { m/$criterion/ };
+	    } elsif ( ref ) {
+		Carp::croak( ref, ' reference not supported' );
+	    } else {
+		my $criterion = $_;
+		$_ = sub { index( $_, $criterion ) >= 0 };
+	    }
+	}
+	my @rslt;
+	foreach my $month ( 0 .. $#on_date ) {
+	    $on_date[$month]
+		or next;
+	    foreach my $day ( 0 .. @{ $on_date[$month] } - 1 ) {
+		local $_ = $on_date[$month][$day]
+		    or next;
+		foreach my $criterion ( @criteria ) {
+		    $criterion->()
+			or next;
+		    push @rslt, [ $month, $day ];
+		    last;
+		}
+	    }
+	}
+
+	return @rslt;
+    }
+
     my @on_date_accented;
+
+    sub _make_on_date_accented {
+
+	# This would be much easier with 'use utf8;', but
+	# unfortunately this was broken under Perl 5.6.
+	my $E_acute	= "\N{LATIN CAPITAL LETTER E WITH ACUTE}";
+	my $e_acute	= "\N{LATIN SMALL LETTER E WITH ACUTE}";
+	my $o_acute	= "\N{LATIN SMALL LETTER O WITH ACUTE}";
+	my $u_acute	= "\N{LATIN SMALL LETTER U WITH ACUTE}";
+	my $u_circ	= "\N{LATIN SMALL LETTER U WITH CIRCUMFLEX}";
+
+	foreach my $month ( @on_date ) {
+	    push @on_date_accented, [];
+	    foreach my $day ( @{ $month } ) {
+		if ( $day ) {
+		    $day =~ s/ \b Anorien \b /An${o_acute}rien/smxgo;
+		    $day =~ s/ \b Annun \b /Ann${u_circ}n/smxgo;
+		    $day =~ s/ \b Barad-dur \b /Barad-d${u_circ}r/smxgo;
+		    $day =~ s/ \b Dunedain \b /D${u_acute}nedain/smxgo;
+		    $day =~ s/ \b Eomer \b /${E_acute}omer/smxgo;
+		    $day =~ s/ \b Eowyn \b /${E_acute}owyn/smxgo;
+		    $day =~ s/ \b Khazad-dum \b /Khazad-d${u_circ}m/smxgo;
+		    $day =~ s/ \b Lorien \b /L${o_acute}rien/smxgo;
+		    $day =~ s/ \b Nazgul \b /Nazg${u_circ}l/smxgo;
+		    $day =~ s/ \b Theoden \b /Th${e_acute}oden/smxgo;
+		    $day =~ s/ \b Theodred \b /Th${e_acute}odred/smxgo;
+		    $day =~ s/ \b Udun \b /Ud${u_circ}n/smxgo;
+		}
+		push @{ $on_date_accented[-1] }, $day;
+	    }
+	}
+	return;
+    }
 
     sub __on_date_accented {
 	my ( $month, $day ) = $validate->( @_ );
 	defined $day
 	    or ( $month, $day ) = ( 0, $month );
 
-	unless ( @on_date_accented ) {
-
-	    # This would be much easier with 'use utf8;', but
-	    # unfortunately this was broken under Perl 5.6.
-	    my $E_acute	= "\N{LATIN CAPITAL LETTER E WITH ACUTE}";
-	    my $e_acute	= "\N{LATIN SMALL LETTER E WITH ACUTE}";
-	    my $o_acute	= "\N{LATIN SMALL LETTER O WITH ACUTE}";
-	    my $u_acute	= "\N{LATIN SMALL LETTER U WITH ACUTE}";
-	    my $u_circ	= "\N{LATIN SMALL LETTER U WITH CIRCUMFLEX}";
-
-	    foreach my $month ( @on_date ) {
-		push @on_date_accented, [];
-		foreach my $day ( @{ $month } ) {
-		    if ( $day ) {
-			$day =~ s/ \b Anorien \b /An${o_acute}rien/smxgo;
-			$day =~ s/ \b Annun \b /Ann${u_circ}n/smxgo;
-			$day =~ s/ \b Barad-dur \b /Barad-d${u_circ}r/smxgo;
-			$day =~ s/ \b Dunedain \b /D${u_acute}nedain/smxgo;
-			$day =~ s/ \b Eomer \b /${E_acute}omer/smxgo;
-			$day =~ s/ \b Eowyn \b /${E_acute}owyn/smxgo;
-			$day =~ s/ \b Khazad-dum \b /Khazad-d${u_circ}m/smxgo;
-			$day =~ s/ \b Lorien \b /L${o_acute}rien/smxgo;
-			$day =~ s/ \b Nazgul \b /Nazg${u_circ}l/smxgo;
-			$day =~ s/ \b Theoden \b /Th${e_acute}oden/smxgo;
-			$day =~ s/ \b Theodred \b /Th${e_acute}odred/smxgo;
-			$day =~ s/ \b Udun \b /Ud${u_circ}n/smxgo;
-		    }
-		    push @{ $on_date_accented[-1] }, $day;
-		}
-	    }
-	}
+	@on_date_accented
+	    or _make_on_date_accented();
 
 	return $on_date_accented[$month][$day];
     }
@@ -1251,10 +1291,10 @@ between C<1> and C<365>, or C<366> in a leap year.
 
 This method formats a date, in a manner similar to C<strftime()>. The
 C<$date> is either an object that supports the necessary methods, or a
-reference to a hash having the necessary keys (same as the
-methods). The C<$pattern> is a string similar to the conversion
-specification passed to C<POSIX::strftime()>; see below for a fuller
-description.
+reference to a hash having the necessary keys (same as the methods,
+minus the C<'__fmt_shire_'> prefix). The C<$pattern> is a string similar
+to the conversion specification passed to C<POSIX::strftime()>; see
+below for a fuller description.
 
 The C<$date> methods used are:
 
@@ -1808,6 +1848,41 @@ since December 31 of proleptic Gregorian year 0) you get back the
 Gregorian year and the day of that year (C<1-366>). If you feed it a
 so-called Shire Rata Die (i.e. days since 1 Yule of Shire year 0) you
 get back the Shire year and the day of that year.
+
+=head2 __search_on_date
+
+ my @dates = __search_on_date( 'depart' );
+ say __on_date( @$_ ) for @dates;
+
+This subroutine takes as its arguments zero or more search criteria, and
+returns an array of Shire dates whose B<unaccented> events match one or
+more of the given criteria.
+
+The Shire dates are represented as references to two-element arrays
+containing the month number and day number, with month zero representing
+a holiday.
+
+The search criteria can be one of the following:
+
+=over
+
+=item * A string
+
+This criterion is satisfied if the event contains the given string.
+
+=item * A Regexp reference
+
+This criterion is satisfied if the event matches the given regexp.
+
+=item * A CODE reference
+
+The code is called with the event in the topic variable (a.k.a. C<$_>).
+This criterion is satisfied if the code returns a true value.
+
+=back
+
+Any other search criterion is invalid, and will result in an exception
+being thrown.
 
 =head2 __trad_weekday_name
 
